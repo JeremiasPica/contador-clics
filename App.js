647632a880456bc733cef5affb1ed0e246af19a2
Jeremías Ezequiel Pica Paredes <104940388+JeremiasPica@users.noreply.github.com>
2024-07-11 19:35:09 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,73 @@
+import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import logoContador from './img/logo.png';
+import { useState } from 'react';
+
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0); 
+
+  const manejarClic = () => {
+      setNumClics(numClics + 1); 
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
+
+  const restarUno = () => {
+    setNumClics(numClics - 1);
+  }
+
+
+  return (
+  <div className='App'>
+    <div className='logo-contenedor'>
+      <img
+        className='logo'
+        src={logoContador}
+        alt='logo de contador de clics' />
+
+    </div>
+
+
+  <div className='contenedor-principal'>
+
+    <Contador 
+    numClics={numClics} 
+    />
+
+    <Boton
+    
+    texto="Clic"
+    esBotonDeClic={true}
+    manejarClic={manejarClic}
+    />
+
+    
+    <Boton 
+
+    texto="Restar 1"
+    esBotonDeClic={false}
+    manejarClic={restarUno}
+    />
+
+    
+      <Boton 
+
+      texto="Reiniciar"
+      esBotonDeClic={false}
+      manejarClic={reiniciarContador}
+      />
+      
+  </div>
+</div>
+
+
+  )
+};
+
+export default App;
